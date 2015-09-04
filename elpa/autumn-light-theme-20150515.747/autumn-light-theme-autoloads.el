54f41faa3b1e04dcbf2f9a2351e78b74df169950
negated <negated@ecik.youhavethewrong.info>
2015-09-04 15:30:54 +0000

    More themes.

---
+++ elpa/autumn-light-theme-20150515.747/autumn-light-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; autumn-light-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "autumn-light-theme" "autumn-light-theme.el"
+;;;;;;  (21993 47213 5288 876000))
+;;; Generated autoloads from autumn-light-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; autumn-light-theme-autoloads.el ends here
---
+++ elpa/green-phosphor-theme-20150515.747/green-phosphor-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "green-phosphor-theme" "20150515.747" "A light color theme with muted, autumnal colors." 'nil :url "http://github.com/aalpern/emacs-color-theme-green-phosphor" :keywords '("color" "theme"))
