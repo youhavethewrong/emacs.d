f1702ef51dc838fe0b02cd79d619eb160754ff37
esc <esc@youhavethewrong.info>
2015-11-16 16:29:10 -0600

    Make .m files be opened in octave mode.

--- customizations/setup-misc-programming.el
+++ customizations/setup-misc-programming.el
@@ -6,3 +6,6 @@
 ;; ;; regular auto-complete initialization
 ;;  (require 'auto-complete-config)
 ;;  (ac-config-default)
+
+;; .m is for octave, not ObjC
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
