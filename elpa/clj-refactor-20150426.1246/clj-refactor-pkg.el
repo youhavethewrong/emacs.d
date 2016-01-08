1d4a52fe9b1b9fd6b1bb8d279865d60bd756bb2f
negated <negated@ecik.youhavethewrong.info>
2016-01-08 15:06:20 +0000

    Updated lots of packages and only using melpa now.

--- elpa/clj-refactor-20150426.1246/clj-refactor-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clj-refactor" "20150426.1246" "A collection of clojure refactoring functions" '((s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "25beta") (multiple-cursors "1.2.2") (cider "0.8.1")) :keywords '("convenience"))
--- init.el
+++ init.el
@@ -2,16 +2,9 @@
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t )
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
-
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
