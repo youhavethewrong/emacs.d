801e595254c8b6df513e1f564f150323e5dae2e6
ESC <erick.crager@qg.com>
2016-03-04 08:58:21 -0600

    Turn on auto-complete-mode when using groovy mode.

--- customizations/setup-groovy.el
+++ customizations/setup-groovy.el
@@ -2,4 +2,5 @@
 (add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
 (add-hook 'groovy-mode-hook 'groovy-electric-mode)
 (add-hook 'groovy-mode-hook 'subword-mode)
+(add-hook 'groovy-mode-hook 'auto-complete-mode)
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
