11df4e1aa3d61fa598787b0d50c5ea21158c9dc1
ESC <youhavethewrong@gmail.com>
2016-03-08 09:33:09 -0600

    Add mode for editing velocity templates.

--- customizations/setup-misc-programming.el
+++ customizations/setup-misc-programming.el
@@ -9,3 +9,7 @@
 
 ;; .m is for octave, not ObjC
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
+
+;; .vm is for velocity templates
+(require 'vtl)
+(add-to-list 'auto-mode-alist '("\\.vm$" . turn-on-vtl-mode))
