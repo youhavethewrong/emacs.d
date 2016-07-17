8d28b8d5b36def20e6791c6d164468dbdfd6f72f
ESC <youhavethewrong@gmail.com>
2016-07-17 11:13:38 -0500

    Add erlang and elixir modes

---
+++ elpa/elixir-yasnippets-0.0.1/elixir-yasnippets-pkg.el
@@ -0,0 +1,5 @@
+(define-package "elixir-yasnippets" "0.0.1" "Yasnippets for Elixir"
+  '((yasnippet "0.8.0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/erlang-19.0.2/erlang-pkg.el
@@ -0,0 +1,4 @@
+(define-package "erlang" "19.0.2" "Erlang major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
