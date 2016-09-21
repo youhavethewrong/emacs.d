06940a941997b6b751fcf0acc37bcc9fa9387fff
ESC <youhavethewrong@gmail.com>
2016-09-21 10:23:31 -0500

    Remove groovy electric mode as it's hard to get used to.  Only bother with webkit if we're running 25.1

--- customizations/setup-groovy.el
+++ customizations/setup-groovy.el
@@ -1,6 +1,5 @@
 ;; groovy / grails
 (add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
-(add-hook 'groovy-mode-hook 'groovy-electric-mode)
 (add-hook 'groovy-mode-hook 'subword-mode)
 (add-hook 'groovy-mode-hook 'auto-complete-mode)
 (add-hook 'groovy-mode-hook
