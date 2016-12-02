a72237e2e26625acb16ea6c41d7d8808678405bc
ESC <youhavethewrong@gmail.com>
2016-12-02 15:26:55 +0000

    Update several packages

---
+++ elpa/clojure-cheatsheet-20161004.2328/clojure-cheatsheet-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-cheatsheet" "20161004.2328" "The Clojure Cheatsheet for Emacs" '((helm "1.7.7") (cider "0.9.0")) :url "https://github.com/clojure-emacs/clojure-cheatsheet" :keywords '("clojure" "cider" "cheatsheet" "helm"))
