01da5ac9ae70c1ec607e0a56a397f6d05464c304
ESC <youhavethewrong@gmail.com>
2017-01-26 09:40:27 -0600

    Set option on cljr to not favor prefix notation in ns cleanup.  Set color for column marker to something nice.

--- init.el
+++ init.el
@@ -184,6 +184,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-firefox))
+ '(cljr-favor-prefix-notation nil)
  '(coffee-tab-width 2)
  '(custom-safe-themes
    (quote
@@ -193,4 +194,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(column-marker-3 ((t (:background "peach puff")))))
