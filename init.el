3bef08e7701cdbe2801406e5ade49d789c1d4c92
mike <mike706574@gmail.com>
2017-01-31 08:10:10 -0600

    Add function for toggling home row key restriction.

--- init.el
+++ init.el
@@ -195,3 +195,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-marker-3 ((t (:background "peach puff")))))
+
+(require 'elm-mode)
