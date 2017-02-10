e8fdbc6672420885c1e7507328bd3da9383274b8
ESC <youhavethewrong@gmail.com>
2017-02-10 21:58:30 +0000

    Define some fsharp settings

--- customizations/setup-fsharp.el
+++ customizations/setup-fsharp.el
@@ -1,2 +1,4 @@
 (require 'fsharp-mode)
 (add-to-list 'auto-mode-alist '("\\.fs[iylx]?$" . fsharp-mode))
+(setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
+(setq fsharp-compiler "/usr/bin/fsharpc")
