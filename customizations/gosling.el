7c92b36663edb52f6356fcdd5581242ae64a75fd
mike <mike706574@gmail.com>
2017-03-21 23:27:49 -0500

    Clean up unused plugins. Separate work-specific and miscellaneous commands.

--- customizations/gosling.el
+++
@@ -1,8 +0,0 @@
-;; TODO: Remember last command!
-(defun gosling (args)
-  (interactive "sEnter args: ")
-  (shell-command (concat "gosling " args)))
-
-(defun gosling-region ()
-  (interactive)
-  (shell-command (concat "gosling " (buffer-substring (region-beginning) (region-end)))))
