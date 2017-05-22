4958b33a57c4667822429898459f27ac6143ca86
ESC <youhavethewrong@gmail.com>
2017-05-22 17:58:39 +0000

    Add packages because work proxy is annoying.

---
+++ elpa/js-auto-beautify-20161030.2209/js-auto-beautify-autoloads.el
@@ -0,0 +1,22 @@
+;;; js-auto-beautify-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "js-auto-beautify" "js-auto-beautify.el" (22819
+;;;;;;  9655 687724 633000))
+;;; Generated autoloads from js-auto-beautify.el
+
+(autoload 'js-auto-beautify-mode "js-auto-beautify" "\
+auto-beautify you js/jsx
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; js-auto-beautify-autoloads.el ends here
