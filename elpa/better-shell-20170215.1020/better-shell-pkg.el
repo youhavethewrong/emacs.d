d2762f2b63a3c1ca9e8bb39999d92e342e16cec4
ESC <youhavethewrong@gmail.com>
2017-06-06 18:22:45 +0000

    Adding elpa packages because installing from work sucks.

---
+++ elpa/better-shell-20170215.1020/better-shell-pkg.el
@@ -0,0 +1,1 @@
+(define-package "better-shell" "20170215.1020" "Better shell management" '((emacs "24.4")) :commit "4ee06b8791f7363a19109d9ea6c5ee95ce998d33" :url "https://github.com/killdash9/better-shell" :keywords '("convenience"))
