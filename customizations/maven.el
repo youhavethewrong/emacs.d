be5aea4e4a04bcbb9ab3b4feb23349d00c67bfb4
ESC <youhavethewrong@gmail.com>
2017-10-12 16:04:31 -0500

    Maven 3.5 has colorful output if you don't put it in batch mode.

--- customizations/maven.el
+++ customizations/maven.el
@@ -8,43 +8,43 @@
 
 (defun mvn-build ()
   (interactive)
-  (mvn-in-project (lambda (d) (compile "mvn clean install"))))
+  (mvn-in-project (lambda (d) (compile "mvn -B clean install"))))
 
 (defun mvn-updates ()
   (interactive)
-  (mvn-in-project (lambda (d) (compile "mvn versions:display-dependency-updates"))))
+  (mvn-in-project (lambda (d) (compile "mvn -B versions:display-dependency-updates"))))
 
 (defun mvn-tree ()
   (interactive)
-  (mvn-in-project (lambda (d) (compile "mvn dependency:tree"))))
+  (mvn-in-project (lambda (d) (compile "mvn -B dependency:tree"))))
 
 (defun mvn-tree-verbose ()
   (interactive)
-  (mvn-in-project (lambda (d) (compile "mvn dependency:tree -Dverbose=true"))))
+  (mvn-in-project (lambda (d) (compile "mvn -B dependency:tree -Dverbose=true"))))
 
 (defun mvn-all-tests ()
   (interactive)
   (mvn-in-project
-   (lambda (d) (compile "mvn test"))))
+   (lambda (d) (compile "mvn -B test"))))
 
 (defun mvn-suite ()
   (interactive)
   (mvn-in-project
    (lambda (d)
      (let ((test-name (file-name-base (buffer-file-name))))
-       (compile (concat "mvn -Dtest=" test-name " test"))))))
+       (compile (concat "mvn -B -Dtest=" test-name " test"))))))
 
 (defun mvn-clojure-test ()
   (interactive)
   (mvn-in-project
    (lambda (d)
-     (compile "mvn compile && mvn clojure:test"))))
+     (compile "mvn -B compile && mvn clojure:test"))))
 
 (defun mvn-compile ()
   (interactive)
   (mvn-in-project
    (lambda (d)
-     (compile (concat "mvn compile")))))
+     (compile (concat "mvn -B compile")))))
 
 (defvar mvn-test-history nil)
 (defun mvn-test (test)
@@ -59,7 +59,7 @@
   (mvn-in-project
    (lambda (d)
      (let ((test-name (file-name-base (buffer-file-name))))
-       (compile (concat "mvn -Dtest=" test-name "#" test " test"))))))
+       (compile (concat "mvn -B -Dtest=" test-name "#" test " test"))))))
 
 (defvar mvn-grep-history nil)
 (defun mvn-grep (term)
