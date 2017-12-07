0dcab577cdb0b4cade669f5fe3adad3e84d96ece
ESC <youhavethewrong@gmail.com>
2017-12-07 07:12:30 -0600

    Update packages.

--- elpa/espresso-theme-20170716.813/espresso-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "espresso-theme" "20170716.813" "Espresso Tutti Colori port for Emacs" 'nil :commit "e79c5c14732b7e34aab75dbcc968839536536c59" :url "https://github.com/dgutov/espresso-theme")
--- elpa/js-auto-beautify-20161030.2209/js-auto-beautify-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; js-auto-beautify-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "js-auto-beautify" "js-auto-beautify.el" (23041
-;;;;;;  56965 505721 97000))
-;;; Generated autoloads from js-auto-beautify.el
-
-(autoload 'js-auto-beautify-mode "js-auto-beautify" "\
-auto-beautify you js/jsx
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; js-auto-beautify-autoloads.el ends here
