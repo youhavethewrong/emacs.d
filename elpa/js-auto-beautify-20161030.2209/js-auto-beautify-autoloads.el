ace60ed8c17d5c906698cb82da28b76bd2b4f205
ESC <youhavethewrong@gmail.com>
2017-12-08 10:26:56 -0600

    Update a few more packages.  Still have to modify clj-refactor to not require seq-25 for some reason.

---
+++ elpa/js-auto-beautify-20161030.2209/js-auto-beautify-autoloads.el
@@ -0,0 +1,22 @@
+;;; js-auto-beautify-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "js-auto-beautify" "js-auto-beautify.el" (23082
+;;;;;;  47289 734658 476000))
+;;; Generated autoloads from js-auto-beautify.el
+
+(autoload 'js-auto-beautify-mode "js-auto-beautify" "\
+auto-beautify you js/jsx
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; js-auto-beautify-autoloads.el ends here
