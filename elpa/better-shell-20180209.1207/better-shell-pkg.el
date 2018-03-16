8ab217cc97e59f7fcd0c6e69a75caa1af8cddb78
ESC <youhavethewrong@gmail.com>
2018-03-16 09:24:22 -0500

    Upgrade lots of packages.

---
+++ elpa/better-shell-20180209.1207/better-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "better-shell" "20180209.1207" "Better shell management" '((emacs "24.4")) :commit "2e9f2f3e7945c27988c50d8de43e681662d6a467" :url "https://github.com/killdash9/better-shell" :keywords '("convenience"))
