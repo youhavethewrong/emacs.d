de0ea4cf442d2fd87da457fafa41557b25006823
ESC <youhavethewrong@gmail.com>
2018-03-20 08:48:45 -0500

    Remove stuff copied from setup-fsharp.el.

--- customizations/setup-scala.el
+++ customizations/setup-scala.el
@@ -1,4 +1,2 @@
 (require 'scala-mode)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-(setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
-(setq fsharp-compiler "/usr/bin/fsharpc")
