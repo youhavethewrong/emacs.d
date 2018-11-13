04cc630991a09edd93005c9df9254875f2a416d7
ESC <youhavethewrong@gmail.com>
2018-11-13 16:41:44 -0600

    Add shortcut for cycling through all styles.

--- customizations/setup-misc-programming.el
+++ customizations/setup-misc-programming.el
@@ -34,3 +34,7 @@
 (remove-hook 'server-switch-hook 'magit-commit-diff)
 ;; disable vc for git.  magit is plenty.
 (setq vc-handled-backends (delq 'Git vc-handled-backends))
+
+(require 'string-inflection)
+;; cycle through all string inflection styles
+(global-set-key (kbd "C-c C-u") 'string-inflection-all-cycle)
