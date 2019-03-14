d7b7bb151d6d49927fe47a1b95e870dfb6cdf5a6
ESC <youhavethewrong@gmail.com>
2019-03-14 13:49:22 -0500

    Turn on selective-display (code folding) with f9.

--- customizations/editing.el
+++ customizations/editing.el
@@ -150,3 +150,16 @@
 
 (global-set-key (kbd "C-M-i") 'paredit-backward-barf-sexp)
 (global-set-key (kbd "C-M-o") 'paredit-forward-barf-sexp)
+
+
+(global-set-key (kbd "<f9>") 'set-selective-display-dlw)
+
+(defun set-selective-display-dlw (&optional level)
+"Fold text indented same of more than the cursor.
+If level is set, set the indent level to LEVEL.
+If 'selective-display' is already set to LEVEL, clicking
+F5 again will unset 'selective-display' by setting it to 0."
+  (interactive "P")
+  (if (eq selective-display (1+ (current-column)))
+      (set-selective-display 0)
+(set-selective-display (or level (1+ (current-column))))))
