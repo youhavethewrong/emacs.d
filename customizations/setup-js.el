86875f8fcfad9877d2ab68a6d79f19188a8914ef
ESC <youhavethewrong@gmail.com>
2019-05-02 15:57:40 -0500

    Haven't used coffeescript in a long time. Defining company mode shortcut.

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -7,11 +7,12 @@
 (add-hook 'js-mode-hook 'indium-interaction-mode)
 (setq js2-basic-offset 2)
 (setq js-indent-level 2)
+(define-key js-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
 
 ;; jsx
 (add-to-list 'auto-mode-alist '("\\.jsx$" . rjsx-mode))
 (add-hook 'rjsx-mode 'prettier-js-mode)
-
 
 ;; html
 (add-hook 'html-mode-hook 'subword-mode)
@@ -20,14 +21,3 @@
      (require 'tagedit)
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
-
-;; coffeescript
-(add-to-list 'auto-mode-alist '("\\.coffee.erb$" . coffee-mode))
-(add-hook 'coffee-mode-hook 'subword-mode)
-(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'coffee-mode-hook
-          (defun coffee-mode-newline-and-indent ()
-            (define-key coffee-mode-map "\C-j" 'coffee-newline-and-indent)
-            (setq coffee-cleanup-whitespace nil)))
-(custom-set-variables
- '(coffee-tab-width 2))
