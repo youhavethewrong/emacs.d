3a3a9103e60bd864836df68729e5334947eb14d7
ESC <youhavethewrong@gmail.com>
2019-05-30 11:09:30 -0500

    Set company mode keybinding after loading js-mode. Add flychecking.

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,18 +1,20 @@
-(require 'indium)
+(require 'flycheck)
 
 ;; javascript
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'js-mode-hook 'prettier-js-mode)
-(add-hook 'js-mode-hook 'indium-interaction-mode)
 (add-hook 'js-mode-hook (lambda () (auto-complete-mode 0)))
 (add-hook 'js-mode-hook 'company-mode)
 (add-hook 'js-mode-hook 'electric-pair-mode)
+(add-hook 'js-mode-hook 'flycheck-mode)
 
 (setq js2-basic-offset 2)
 (setq js-indent-level 2)
-(define-key js-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
+(eval-after-load "js-mode"
+  '(progn
+     (define-key js-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+     (setq company-tooltip-align-annotations t)))
 
 ;; jsx
 (add-to-list 'auto-mode-alist '("\\.jsx$" . rjsx-mode))
