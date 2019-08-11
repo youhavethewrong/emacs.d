31ce271ea90a4e2f84bfeef879b1f664abf7d3a1
ESC <youhavethewrong@gmail.com>
2019-08-11 13:38:59 -0500

    Upgrade clojure packages. I've been neglecting them.

--- elpa/clojure-mode-extra-font-locking-20180114.911/clojure-mode-extra-font-locking-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode-extra-font-locking" "20180114.911" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "d1e0a6e99f8a98cc2976a23e856efa6d25283f23" :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "lisp"))
