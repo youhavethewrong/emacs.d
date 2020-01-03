435d039c7dd238948b8d15cf5b53dc7b0f7b96af
ESC <youhavethewrong@gmail.com>
2020-01-02 21:10:02 -0600

    Set up LSP with flycheck and company mode. Works pretty well with rust!

--- customizations/programming.el
+++ customizations/programming.el
@@ -23,13 +23,43 @@
   (global-set-key (kbd "C-c C-u") 'string-inflection-all-cycle))
 
 (use-package lsp-mode
+  :config
+  (setq lsp-prefer-flymake nil)
   :hook
   (rust-mode . lsp)
   :commands lsp)
 
-(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ui
+  :requires lsp-mode flycheck
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-position 'top
+        lsp-ui-doc-include-signature t
+        lsp-ui-sideline-enable nil
+        lsp-ui-flycheck-enable t
+        lsp-ui-flycheck-list-position 'right
+        lsp-ui-flycheck-live-reporting t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-list-width 60
+        lsp-ui-peek-peek-height 25)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
-(use-package company-lsp :commands company-lsp)
+(use-package company
+  :config
+  (setq company-idle-delay 0.3)
+  (global-company-mode 1)
+  (global-set-key (kbd "C-<tab>") 'company-complete))
+
+(use-package company-lsp
+  :requires company
+  :config
+  (push 'company-lsp company-backends)
+
+   ;; Disable client-side cache because the LSP server does a better job.
+  (setq company-transformers nil
+        company-lsp-async t
+        company-lsp-cache-candidates nil))
 
 ;; .m is for octave, not ObjC
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
