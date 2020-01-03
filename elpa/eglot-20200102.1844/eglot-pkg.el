4700e63f08a2f127728e312b5e0381eedcfd58ee
ESC <youhavethewrong@gmail.com>
2020-01-02 21:05:21 -0600

    Upgrade all deps.

---
+++ elpa/eglot-20200102.1844/eglot-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eglot" "20200102.1844" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.7") (flymake "1.0.5")) :commit "4b453dcde984881e648f1c17c2fc7b147fce7f5b" :keywords '("convenience" "languages") :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :url "https://github.com/joaotavora/eglot")
