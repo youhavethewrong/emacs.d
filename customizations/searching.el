4e2b4130adec130e2d6eaba88df7e9582f9b680e
ESC <youhavethewrong@gmail.com>
2020-09-28 08:30:04 -0500

    Ignore node_modules and random json.

--- customizations/searching.el
+++ customizations/searching.el
@@ -143,7 +143,7 @@
 
 (defvar search-tool
   (if (executable-find "rg")
-      "rg -i -H --no-heading -e \""
+      "rg -i -H --no-heading -g '!node_modules' -g !'*.json' -e \""
     "grep --color --exclude-dir=.git --exclude-dir=public --exclude-dir=build --exclude-dir=reference --exclude-dir=resources --exclude-dir=coverage --exclude-dir=node_modules -nriH -e \""))
 
 (defun npm-grep (term)
