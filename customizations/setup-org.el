9579df94afaad18601fc972fd659b06396670c97
ESC <youhavethewrong@gmail.com>
2020-10-05 08:24:44 -0500

    Also load visual line mode with org-bullet mode. Bigger on MAc.

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -2,4 +2,5 @@
 (use-package org-bullets
   :defer t
   :init
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (add-hook 'org-mode-hook visual-line-mode-hook))
