efd5dde94e98615ec94348905913e35a6c52fb73
ESC <erick@youhavethewrong.info>
2021-03-03 07:49:27 -0600

    Clean up some obsolete packages and use a different font on Mac

--- customizations/ui.el
+++ customizations/ui.el
@@ -18,7 +18,7 @@
 ;; Make the font size a bit smaller on Linux, bigger on Mac
 ;; (-filter (lambda (font) (string-suffix-p "Mono" font)) (font-family-list))
 (if (memq window-system '(mac ns))
-    (set-face-attribute 'default nil :family "FuraCode Nerd Font Mono" :height 130 :weight 'regular :width 'regular)
+    (set-face-attribute 'default nil :family "FiraCode Nerd Font Mono" :height 110 :weight 'regular :width 'regular)
   (set-face-attribute 'default nil :family "Fira Mono" :height 100 :weight 'regular :width 'regular))
 
 ;; Go straight to scratch buffer on startup
