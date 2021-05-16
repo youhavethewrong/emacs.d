f9408f05a2eb665a563a6fc41e0a70133664e728
ESC <erick@youhavethewrong.info>
2021-05-16 08:09:35 -0500

    Update org setup to use latest syntax consistently.

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -1,12 +1,12 @@
-;; loaded in init.el
-(require 'org)
-
-(use-package org-bullets
-  :defer t
+(use-package org
   :init
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  (add-hook 'org-mode-hook visual-line-mode-hook)
+  (setq org-adapt-indentation nil)
+  :config
   (add-to-list 'org-structure-template-alist '("r" . "src rust
 "))
   (add-to-list 'org-structure-template-alist '("j" . "src json
 ")))
+
+(use-package org-bullets
+  :defer t
+  :hook (org-mode . org-bullets-mode))
