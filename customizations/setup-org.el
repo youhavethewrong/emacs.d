66bf4c475a599f2e6fdfefa30e0fd62fc06253f1
Erick Crager <erick@youhavethewrong.info>
2023-05-12 13:49:20 -0500

    Support fitness too

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -11,12 +11,28 @@
   :defer t
   :hook (org-mode . org-bullets-mode))
 
-(defun esc/dated-template ()
-  (concat (format "\n* %s \n**" (format-time-string "%B%e, %Y")) " %?"))
+;; Read this in from a file eventually - use blank for 'other'
+(defvar maintenance-completions '("" "1989 Ranch House" "2022 Aprilia Tuono 660 Factory" "2017 Subaru Outback"))
+
+(defun esc/choose-maint-item ()
+  "Read in a durable good from the user."
+  (interactive)
+  (let ((choice (completing-read "Choose: " maintenance-completions nil t)))
+    choice))
+
+(defun esc/dated-template (choice-fn-string)
+  (concat (format "\n* %s \n** "
+                  (format-time-string "%B %e, %Y"))
+          " "
+          choice-fn-string
+          "%?"))
 
 (setq org-capture-templates
       '(
         ("m" "Maintenance" entry (file "~/lab-journal/maintenance.org")
-         (function esc/dated-template)
+         (function (lambda () (esc/dated-template "%(esc/choose-maint-item)")))
+         )
+        ("f" "Fitness" entry (file "~/lab-journal/fitness.org")
+         (function (lambda () (esc/dated-template "")))
          )
         ))
