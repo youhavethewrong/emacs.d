66cde52bc88c653973d610407ddb7439cf4e6ea1
ESC <erick@youhavethewrong.info>
2023-10-16 20:18:10 -0500

    Eyes are getting old I guess

--- customizations/ui.el
+++ customizations/ui.el
@@ -19,7 +19,7 @@
 ;;(-filter (lambda (font) (string-suffix-p "Nerd Font" font)) (font-family-list))
 (if (memq window-system '(mac ns))
     (set-face-attribute 'default nil :family "FiraCode Nerd Font" :height 110 :weight 'regular :width 'regular)
-  (set-face-attribute 'default nil :family "Fira Mono" :height 80 :weight 'regular :width 'regular))
+  (set-face-attribute 'default nil :family "Fira Mono" :height 100 :weight 'regular :width 'regular))
 
 ;; Go straight to scratch buffer on startup
 (setq inhibit-startup-message t)
