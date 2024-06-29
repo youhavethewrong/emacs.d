fa3a8b2751007a645e0ee6f3e25a65d7bd9b25d1
ESC <erick@youhavethewrong.info>
2024-06-29 08:41:42 -0500

    Add elfeed for RSS/Atom reading

--- customizations/setup-misc.el
+++ customizations/setup-misc.el
@@ -1,8 +1,14 @@
 (use-package hledger-mode
   :defer t)
 
+;; Gemini/Gopher reader
 (use-package elpher
   :defer t)
+
+;; RSS/Atom reader
+(use-package elfeed
+  :defer t
+  :pin melpa-stable)
 
 (use-package esup
   :ensure t
